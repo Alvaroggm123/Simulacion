bbd480104458ab2eca8aaf4138324d61e2d231c2
Alvaro Gonzalez <aggm123@icloud.com>
2021-04-21 20:35:43 -0700

    Se incorporó el color al PCB.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Simulacion
 {
@@ -23,16 +24,19 @@ namespace Simulacion
     public class PCB
     {
         // Campos de clase
+        public Color pcbColor;
         public string pcbName, pcbUser, pcbState;
         public int pcbPID, pcbMemory = 0;
         // Constructor con parametros de entrada
         public PCB(int pcbPID, string pcbName, string pcbUser, string pcbState, int pcbMemory)
         {
+            Random Ran = new Random();
             this.pcbPID = pcbPID;
             this.pcbName = pcbName;
             this.pcbMemory = pcbMemory;
             this.pcbUser = pcbUser;
             this.pcbState = pcbState;
+            pcbColor = Color.FromArgb(Ran.Next(0, 255), Ran.Next(0, 255), Ran.Next(0, 255));
         }
     }
 }
