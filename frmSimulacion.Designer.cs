ea87e5be84dfa7c4638deddc16f71413bf76b868
Alvaro Gonzalez <aggm123@icloud.com>
2021-04-22 18:06:43 -0700

    Se generaron cambios en listas e impresión.

--- frmSimulacion.Designer.cs
+++ frmSimulacion.Designer.cs
@@ -30,6 +30,15 @@ namespace Simulacion
         private void InitializeComponent()
         {
             this.grpbProcesos = new System.Windows.Forms.GroupBox();
+            this.lblPCB = new System.Windows.Forms.Label();
+            this.tabProcesos = new System.Windows.Forms.ListView();
+            this.PID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Memoria = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Usuario = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Estado = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
             this.tackQuantum = new System.Windows.Forms.TrackBar();
             this.cmdProcesar = new System.Windows.Forms.Button();
             this.cmdAgregar = new System.Windows.Forms.Button();
@@ -38,16 +47,7 @@ namespace Simulacion
             this.txtProcessN = new System.Windows.Forms.TextBox();
             this.lblProcessN = new System.Windows.Forms.Label();
             this.panelLoading = new System.Windows.Forms.Panel();
-            this.lblTitulo = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.tabProcesos = new System.Windows.Forms.ListView();
-            this.PID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Memoria = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Usuario = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Estado = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.lblPCB = new System.Windows.Forms.Label();
             this.grpbProcesos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tackQuantum)).BeginInit();
             this.SuspendLayout();
@@ -67,10 +67,79 @@ namespace Simulacion
             this.grpbProcesos.Controls.Add(this.lblProcessN);
             this.grpbProcesos.Location = new System.Drawing.Point(12, 12);
             this.grpbProcesos.Name = "grpbProcesos";
-            this.grpbProcesos.Size = new System.Drawing.Size(776, 187);
+            this.grpbProcesos.Size = new System.Drawing.Size(776, 174);
             this.grpbProcesos.TabIndex = 0;
             this.grpbProcesos.TabStop = false;
             this.grpbProcesos.Text = "Simulacion de procesos";
+            // 
+            // lblPCB
+            // 
+            this.lblPCB.AutoSize = true;
+            this.lblPCB.Location = new System.Drawing.Point(471, 19);
+            this.lblPCB.Name = "lblPCB";
+            this.lblPCB.Size = new System.Drawing.Size(89, 13);
+            this.lblPCB.TabIndex = 8;
+            this.lblPCB.Text = "PCB del proceso.";
+            this.lblPCB.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tabProcesos
+            // 
+            this.tabProcesos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.PID,
+            this.Nombre,
+            this.Memoria,
+            this.Usuario,
+            this.Estado});
+            this.tabProcesos.HideSelection = false;
+            this.tabProcesos.Location = new System.Drawing.Point(286, 35);
+            this.tabProcesos.Name = "tabProcesos";
+            this.tabProcesos.Size = new System.Drawing.Size(484, 128);
+            this.tabProcesos.TabIndex = 9;
+            this.tabProcesos.UseCompatibleStateImageBehavior = false;
+            this.tabProcesos.View = System.Windows.Forms.View.Details;
+            // 
+            // PID
+            // 
+            this.PID.Text = "Identificador";
+            this.PID.Width = 71;
+            // 
+            // Nombre
+            // 
+            this.Nombre.Text = "Nombre";
+            this.Nombre.Width = 86;
+            // 
+            // Memoria
+            // 
+            this.Memoria.Text = "Memoria";
+            this.Memoria.Width = 53;
+            // 
+            // Usuario
+            // 
+            this.Usuario.Text = "Usuario";
+            // 
+            // Estado
+            // 
+            this.Estado.Text = "Estado";
+            this.Estado.Width = 82;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Quantums:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(57, 19);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(108, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Nombre del proceso: ";
             // 
             // tackQuantum
             // 
@@ -90,6 +159,7 @@ namespace Simulacion
             this.cmdProcesar.TabIndex = 7;
             this.cmdProcesar.Text = "Procesar";
             this.cmdProcesar.UseVisualStyleBackColor = true;
+            this.cmdProcesar.Click += new System.EventHandler(this.cmdProcesar_Click);
             // 
             // cmdAgregar
             // 
@@ -135,92 +205,21 @@ namespace Simulacion
             // 
             // panelLoading
             // 
-            this.panelLoading.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panelLoading.CausesValidation = false;
+            this.panelLoading.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panelLoading.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelLoading.Enabled = false;
-            this.panelLoading.Location = new System.Drawing.Point(12, 184);
+            this.panelLoading.Location = new System.Drawing.Point(12, 192);
             this.panelLoading.Name = "panelLoading";
-            this.panelLoading.Size = new System.Drawing.Size(390, 244);
+            this.panelLoading.Size = new System.Drawing.Size(375, 236);
             this.panelLoading.TabIndex = 1;
-            // 
-            // lblTitulo
-            // 
-            this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Location = new System.Drawing.Point(57, 19);
-            this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(108, 13);
-            this.lblTitulo.TabIndex = 0;
-            this.lblTitulo.Text = "Nombre del proceso: ";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(222, 19);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Quantums:";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // panel1
             // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.CausesValidation = false;
-            this.panel1.Enabled = false;
-            this.panel1.Location = new System.Drawing.Point(408, 184);
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(413, 192);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(380, 244);
+            this.panel1.Size = new System.Drawing.Size(375, 236);
             this.panel1.TabIndex = 2;
-            // 
-            // tabProcesos
-            // 
-            this.tabProcesos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.PID,
-            this.Nombre,
-            this.Memoria,
-            this.Usuario,
-            this.Estado});
-            this.tabProcesos.HideSelection = false;
-            this.tabProcesos.Location = new System.Drawing.Point(286, 35);
-            this.tabProcesos.Name = "tabProcesos";
-            this.tabProcesos.Size = new System.Drawing.Size(484, 124);
-            this.tabProcesos.TabIndex = 9;
-            this.tabProcesos.UseCompatibleStateImageBehavior = false;
-            this.tabProcesos.View = System.Windows.Forms.View.Details;
-            // 
-            // PID
-            // 
-            this.PID.Text = "Identificador";
-            this.PID.Width = 71;
-            // 
-            // Nombre
-            // 
-            this.Nombre.Text = "Nombre";
-            this.Nombre.Width = 86;
-            // 
-            // Memoria
-            // 
-            this.Memoria.Text = "Memoria";
-            this.Memoria.Width = 53;
-            // 
-            // Usuario
-            // 
-            this.Usuario.Text = "Usuario";
-            // 
-            // Estado
-            // 
-            this.Estado.Text = "Estado";
-            this.Estado.Width = 82;
-            // 
-            // lblPCB
-            // 
-            this.lblPCB.AutoSize = true;
-            this.lblPCB.Location = new System.Drawing.Point(471, 19);
-            this.lblPCB.Name = "lblPCB";
-            this.lblPCB.Size = new System.Drawing.Size(89, 13);
-            this.lblPCB.TabIndex = 8;
-            this.lblPCB.Text = "PCB del proceso.";
-            this.lblPCB.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // frmSimulacion
             // 
@@ -249,11 +248,9 @@ namespace Simulacion
         private System.Windows.Forms.Label lblProcessM;
         private System.Windows.Forms.TextBox txtProcessN;
         private System.Windows.Forms.Label lblProcessN;
-        private System.Windows.Forms.Panel panelLoading;
         private System.Windows.Forms.TrackBar tackQuantum;
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ListView tabProcesos;
         private System.Windows.Forms.ColumnHeader PID;
         private System.Windows.Forms.ColumnHeader Nombre;
@@ -261,6 +258,8 @@ namespace Simulacion
         private System.Windows.Forms.ColumnHeader Usuario;
         private System.Windows.Forms.ColumnHeader Estado;
         private System.Windows.Forms.Label lblPCB;
+        private System.Windows.Forms.Panel panelLoading;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- frmSimulacion.cs
+++ frmSimulacion.cs
@@ -7,17 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Drawing;
 
 namespace Simulacion
 {
     public partial class frmSimulacion : Form
     {
         /* | Variables globales | */
-        List<PCB> Procesos = new List<PCB>();
-        string[] Usuarios = { "root", "aggm123", "aranzagtz" };
+        List<PCB> Process = new List<PCB>();
+        string[] Usuarios = { "root", "aggm123", "aranzagtz", "Anonymous" };
         string[] Ejecutables = { "Firefox", "Explorer", "Virus", "Chrome", "Calculadora", "Apoint", "Agenservice", "System" };
-        int ID = 0, x = 0, y = 0, w = 5, h = 10;
+        int Index = 0, w = 5, h = 10;
+        int x = 0, y = 0;
+
+        private void cmdProcesar_Click(object sender, EventArgs e)
+        {
+            // Validamos que existan elementos en la lista
+            if (Index >= 2)
+            {
+                int i = 0;
+                List<PCB> ProcesosOrd = Process.ToList();
+                ProcesosOrd.Sort((p, q) => p.pcbMemory.CompareTo(q.pcbMemory));
+                Dibuja(ProcesosOrd, panel1);
+            }
+            else
+            {
+
+            }
+        }
+
         Pen pen;
         /* | Fin Variables globales | */
         public frmSimulacion()
@@ -28,8 +45,6 @@ namespace Simulacion
 
         private void cmdAgregar_Click(object sender, EventArgs e)
         {
-            // Elemento que permite que nos comuniquemos con el panel
-            Graphics G = panelLoading.CreateGraphics();
             // Generación de numeros aleatorios
             Random Ran = new Random();
 
@@ -40,38 +55,28 @@ namespace Simulacion
             if (txtProcessM.Text == "")
                 txtProcessM.Text = Convert.ToString(Ran.Next(1, 10));
             // Agregamos nuevo elemento a la lista del PCB
-            Procesos.Add( new PCB(
-                ID,
+            Process.Add(new PCB(
+                Index,
                 txtProcessN.Text, Usuarios[Ran.Next(0, 3)],
                 "new",
                 Convert.ToInt32(txtProcessM.Text)));
+            // Llamamos al evento que imprime los rectangulos
+            Dibuja(Process, panelLoading);
+
+
             // Agregamos los elementos a la lista (Tabla)
             // Definición de la listView
-            ListViewItem Elemento =new ListViewItem(Convert.ToString(Procesos[ID].pcbPID));
+            ListViewItem Elemento = new ListViewItem(Convert.ToString(Process[Index].pcbPID));
 
-            Elemento.SubItems.Add(Convert.ToString(Procesos[ID].pcbName));
-            Elemento.SubItems.Add(Convert.ToString(Procesos[ID].pcbMemory));
-            Elemento.SubItems.Add(Convert.ToString(Procesos[ID].pcbUser));
-            Elemento.SubItems.Add(Convert.ToString(Procesos[ID].pcbState));
-            Elemento.BackColor = Procesos[ID].pcbColor;
+            Elemento.SubItems.Add(Convert.ToString(Process[Index].pcbName));
+            Elemento.SubItems.Add(Convert.ToString(Process[Index].pcbMemory));
+            Elemento.SubItems.Add(Convert.ToString(Process[Index].pcbUser));
+            Elemento.SubItems.Add(Convert.ToString(Process[Index].pcbState));
+            Elemento.BackColor = Process[Index].pcbColor;
             tabProcesos.Items.Add(Elemento);
-            //tabProcesos.Rows[ID].ContextMenuStrip = Procesos[ID].pcbColor;
 
-            Pen Lapiz = new Pen(Procesos[ID].pcbColor);
-            Lapiz.Width = 5;
-
-            for (int i = 0; i < Procesos[ID].pcbMemory; i++)
-            {
-                if (x > panelLoading.Width - w * 2)
-                {
-                    y += h * 2 - 3;
-                    x = 0;
-                }
-                G.DrawRectangle(Lapiz, new Rectangle(x, y, w, h));
-                x += w + w / 2 + 4;
-            }
             // Recorremos el apuntador
-            ID++;
+            Index++;
             // Limpiamos las cajas de texto
             foreach (Control Caja in grpbProcesos.Controls)
                 if (Caja is TextBox)
@@ -79,5 +84,33 @@ namespace Simulacion
         }
 
         /* | Terminan los eventos | */
+
+        /* |==| Comienzan los métodos |==| */
+        public void Dibuja(List<PCB> Procesos, Panel panel)
+        {
+            // Elemento que permite que nos comuniquemos con el panel
+            Graphics G = panel.CreateGraphics();
+
+            // Index local
+            int i = 0;
+            foreach(PCB Proceso in Procesos)
+            {
+                Pen Lapiz = new Pen(Procesos[i].pcbColor);
+                Lapiz.Width = 5;
+
+                for (int j = 0; j < Procesos[i].pcbMemory; j++)
+                {
+                    if (x > panelLoading.Width - w * 2)
+                    {
+                        y += h * 2 - 3;
+                        x = 0;
+                    }
+                    G.DrawRectangle(Lapiz, new Rectangle(x, y, w, h));
+                    x += w + w / 2 + 4;
+                }
+                i++;
+            }
+            x = 0; y = x;
+        }
     }
 }
